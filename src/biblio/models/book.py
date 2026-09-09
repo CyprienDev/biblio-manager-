f7be4d355653ddfb95ab95d817c38c1a72de2ba9
Mirage <cyprien.2003@icloud.com>
2026-09-09 12:36:58 +0200

    Ajout du modèle Book

--- src/biblio/models/book.py
+++ src/biblio/models/book.py
@@ -1,1 +1,28 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class Book:
+    id: int
+    titre: str
+    auteur: str
+    isbn: str
+    exemplaires_total: int
+    exemplaires_dispo: int
+
+    def is_available(self) -> bool:
+        return self.exemplaires_dispo > 0
+
+    def borrow_copy(self) -> bool:
+        if not self.is_available():
+            return False
+
+        self.exemplaires_dispo -= 1
+        return True
+
+    def return_copy(self) -> bool:
+        if self.exemplaires_dispo >= self.exemplaires_total:
+            return False
+
+        self.exemplaires_dispo += 1
+        return True
